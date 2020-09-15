bf43074a2384abb8f13efa74028c92e0a2820385
LioshynMaxim <leshinmaxim@gmail.com>
2020-09-15 14:05:08 +0300

    Add lesson 1.1

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -10,10 +10,28 @@ const pi = 3.14;
 
 console.log(second);
 
-// var number = 5;
-// var string = "Hello";
-// var sym = Symbol();
-// var boolean = true;
-// null;
-// undefine;
-// var obj = {};
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+var nullVariable = null;
+// var undefineVariable = undefine;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let smth;
+console.log(smth);
+
+let persone = {
+    name : "Jhone",
+    age : 25,
+    isMarried : false
+};
+
+console.log(persone.name);
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[2]);
