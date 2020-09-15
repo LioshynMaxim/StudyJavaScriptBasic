3aa64a4984c36311b6063a9f40b146bd92a928b5
LioshynMaxim <leshinmaxim@gmail.com>
2020-09-15 13:32:15 +0300

    Add lesson one

---
+++ Lesson1/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var leftBorderWidth = 1;
+{
+    let second = 2; // Area of visibility local
+}
+
+let second = 2; // Area of visibility global
+const pi = 3.14;
+
+console.log(second);
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefine;
+// var obj = {};
